dce44cf6ec02825eceb43a17b33e89b74fba7f49
Yadokame <yadokame3.14@gmail.com>
2022-11-04 20:13:16 +0900

    change block instantiate with block size.

--- Assets/Scripts/InstantiateWall.cs
+++ Assets/Scripts/InstantiateWall.cs
@@ -1,31 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class InstantiateWall : MonoBehaviour{
-    [SerializeField] private GameObject wallBlock;
-    [SerializeField] private GameObject ball;
-    private const int STAGE_WIDTH = 16;
-    private const int STAGE_HEIGHT = 9;
-        
-    // Start is called before the first frame update
-    void Start() {
-        for (float x = 0f; x <= STAGE_WIDTH; x+=0.5f) {
-            for (float y = 0f; y <= STAGE_HEIGHT; y+=0.5f) {
-                if(x == 0f || x == STAGE_WIDTH || y == 0f || y == STAGE_HEIGHT)
-                {
-                    Vector2 blockPos = new Vector2(x - STAGE_WIDTH/2.0f, y - STAGE_HEIGHT/2.0f);
-                    Instantiate(wallBlock, blockPos, Quaternion.identity);
-                }
-            }
-        }
+[SerializeField] private GameObject wallBlock;
+[SerializeField] private GameObject ball;
+private const int STAGE_WIDTH = 16;
+private const int STAGE_HEIGHT = 9;
+private int HRZ_BLOCK_COUNT = 16;//horizontal block count
+private int VRT_BLOCK_COUNT = 9;
 
-        Instantiate(ball, Vector2.zero, Quaternion.identity);
-    }
+// Start is called before the first frame update
+void Start()
+{
+  Vector3 blockScale = wallBlock.GetComponent<Transform>().localScale;
+  HRZ_BLOCK_COUNT = (int)(STAGE_WIDTH / blockScale.x);
+  VRT_BLOCK_COUNT = (int)(STAGE_HEIGHT / blockScale.y);
+  Debug.Log(HRZ_BLOCK_COUNT);
+  for (int xBlockCount = 0; xBlockCount <= HRZ_BLOCK_COUNT; xBlockCount++) {
+    for (int yBlockCount = 0; yBlockCount <= VRT_BLOCK_COUNT; yBlockCount++) {
+      if(xBlockCount == 0 || xBlockCount == HRZ_BLOCK_COUNT || yBlockCount == 0 || yBlockCount == VRT_BLOCK_COUNT)
+      {
+        float xPos = -STAGE_WIDTH / 2.0f + (float)STAGE_WIDTH/HRZ_BLOCK_COUNT*xBlockCount;
+        float yPos = -STAGE_HEIGHT / 2.0f + (float)STAGE_HEIGHT/VRT_BLOCK_COUNT*yBlockCount;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector2 blockPos = new Vector2(xPos, yPos);
+        Instantiate(wallBlock, blockPos, Quaternion.identity);
+      }
     }
+  }
+
+  Instantiate(ball, Vector2.zero, Quaternion.identity);
+}
+
+// Update is called once per frame
+void Update() { }
 }
