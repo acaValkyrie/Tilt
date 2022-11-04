00512a93d702d6b8145c1f38faad64d6d0d5bd97
Valkyrie-freja <yadokame3.14@gmail.com>
2022-11-04 23:31:05 +0900

    add assets. ignore .png extension.

--- Assets/Scripts/InstantiateWall.cs
+++ Assets/Scripts/InstantiateWall.cs
@@ -8,20 +8,21 @@ public class InstantiateWall : MonoBehaviour{
 [SerializeField] private GameObject ball;
 private const int STAGE_WIDTH = 16;
 private const int STAGE_HEIGHT = 9;
-private int HRZ_BLOCK_COUNT = 16;//horizontal block count
-private int VRT_BLOCK_COUNT = 9;
+private static int HRZ_BLOCK_COUNT = 16;//horizontal block count
+private static int VRT_BLOCK_COUNT = 9;
 
 // Start is called before the first frame update
-void Start()
-{
+void Start() {
   Vector3 blockScale = wallBlock.GetComponent<Transform>().localScale;
   HRZ_BLOCK_COUNT = (int)(STAGE_WIDTH / blockScale.x);
   VRT_BLOCK_COUNT = (int)(STAGE_HEIGHT / blockScale.y);
-  Debug.Log(HRZ_BLOCK_COUNT);
+
+  Vector3 ballScale = blockScale * 0.9f;
+  ball.transform.localScale = ballScale;
+  
   for (int xBlockCount = 0; xBlockCount <= HRZ_BLOCK_COUNT; xBlockCount++) {
     for (int yBlockCount = 0; yBlockCount <= VRT_BLOCK_COUNT; yBlockCount++) {
-      if(xBlockCount == 0 || xBlockCount == HRZ_BLOCK_COUNT || yBlockCount == 0 || yBlockCount == VRT_BLOCK_COUNT)
-      {
+      if(xBlockCount == 0 || xBlockCount == HRZ_BLOCK_COUNT || yBlockCount == 0 || yBlockCount == VRT_BLOCK_COUNT) {
         float xPos = -STAGE_WIDTH / 2.0f + (float)STAGE_WIDTH/HRZ_BLOCK_COUNT*xBlockCount;
         float yPos = -STAGE_HEIGHT / 2.0f + (float)STAGE_HEIGHT/VRT_BLOCK_COUNT*yBlockCount;
 
